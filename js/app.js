81469b040cdac03f90a2acd57e9b62864b46912d
Yury Shulaev <yury.shulaev@gmail.com>
2016-05-18 20:14:15 +0300

    Add utils object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let view = new Satori();
+let utils = {arrayRemove: view.arrayRemove};
 let model = view.proxy(new TodosModel((JSON.parse(localStorage.getItem('todos-satori')) || []).map(todo => new Todo(todo))));
 let save = () => {localStorage.setItem('todos-satori', JSON.stringify(view.unproxy(model.todos)))};
 view.onEvent.add(view.throttle(50, save));
