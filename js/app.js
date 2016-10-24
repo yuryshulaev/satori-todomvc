973ddf850bc7c4878d9cf567b605bbae2d145add
Yury Shulaev <yury.shulaev@gmail.com>
2016-10-24 21:41:55 +0300

    New API

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict'
 
-let view = new Satori();
-let utils = {arrayRemove: view.arrayRemove};
-let model = view.proxy(new TodosModel((JSON.parse(localStorage.getItem('todos-satori')) || []).map(todo => new Todo(todo))));
-let save = () => {localStorage.setItem('todos-satori', JSON.stringify(view.unproxy(model.todos)))};
+const view = new Satori();
+const utils = {arrayRemove: view.arrayRemove};
+const model = view.proxy(new TodosModel((JSON.parse(localStorage.getItem('todos-satori')) || []).map(todo => new Todo(todo))));
+const save = () => {localStorage.setItem('todos-satori', JSON.stringify(view.unproxy(model.todos)))};
 view.onEvent.add(view.throttle(50, save));
-let vm = view.proxy(new ViewModel(model, view));
-view.content(view.qs('.content'), TodoAppView(vm, view));
+const vm = view.proxy(new ViewModel(model, view));
+view.content(view.qs('.content'), TodoAppView(vm, view, view.h));
 Router({'/:filter': filter => {vm.filter = filter}}).init();
--- js/todomvc.js
+++ js/todomvc.js
@@ -26,7 +26,7 @@ class ViewModel {
 		this.model = model;
 		this.view = view;
 		this.filter = filter || 'all';
-		let proxy = view.proxy(this);
+		const proxy = view.proxy(this);
 		view.observer('items', () => {proxy.items = model.todos.filter(this.filters.get(proxy.filter).func)});
 	}
 
@@ -44,33 +44,33 @@ class ViewModel {
 	}
 }
 
-let TodoView = (vm, view, todo) => {
+const TodoView = (vm, view, h, todo) => {
 	let edit;
 
-	return view.li({class: {completed: () => todo.completed, editing: () => todo.editing}}, [
-		view.div({class: 'view'}, [
-			view.input({class: 'toggle', attr: {type: 'checkbox'}, bind: {model: todo, key: 'completed'}}),
-			view.label({on: {dblclick() {edit.value = todo.title; vm.startEdit(todo); view.focus(edit)}}}, () => todo.title),
-			view.button({class: 'destroy', on: {click() {vm.model.remove(todo)}}})]),
-		edit = view.input({class: 'edit', on: {blur() {vm.saveEdit(todo, this.value)}},
+	return h('li', {class: {completed: () => todo.completed, editing: () => todo.editing}}, [
+		h('div', {class: 'view'}, [
+			h('input', {class: 'toggle', attr: {type: 'checkbox'}, bind: {model: todo, key: 'completed'}}),
+			h('label', {on: {dblclick() {edit.value = todo.title; vm.startEdit(todo); view.focus(edit)}}}, () => todo.title),
+			h('button', {class: 'destroy', on: {click() {vm.model.remove(todo)}}})]),
+		edit = h('input', {class: 'edit', on: {blur() {vm.saveEdit(todo, this.value)}},
 			keydown: {[view.Key.ENTER]: el => {el.blur()}, [view.Key.ESCAPE]: () => {vm.cancelEdit(todo)}}})]);
 };
 
-let TodoAppView = (vm, view) =>
-	view.section({class: 'todoapp'}, [
-		view.header({class: 'header'}, [
-			view.h1({}, 'todos'),
-			view.input({class: 'new-todo', attr: {placeholder: 'What needs to be done?', autofocus: ''},
+const TodoAppView = (vm, view, h) =>
+	h('section', {class: 'todoapp'}, [
+		h('header', {class: 'header'}, [
+			h('h1', {}, 'todos'),
+			h('input', {class: 'new-todo', attr: {placeholder: 'What needs to be done?', autofocus: ''},
 				keydown: view.inputKeyHandler(value => vm.model.add(value), {reset: true})})]),
-		view.section({class: 'main', show: () => vm.model.todos.length}, [
-			view.input({class: 'toggle-all', attr: {id: 'toggle-all', type: 'checkbox'}, bind: {model: vm, key: 'allCompleted'}}),
-			view.label({attr: {for: 'toggle-all'}}, 'Mark all as complete'),
-			view.ul({class: 'todo-list', list: {array: () => vm.items, item: todo => TodoView(vm, view, todo)}})]),
-		view.footer({class: 'footer', show: () => vm.model.todos.length}, [
-			view.span({class: 'todo-count'}, () => [view.strong({}, vm.remainingCount), ' ', view.pluralize('item', vm.remainingCount), ' ', 'left']),
-			view.ul({class: 'filters'}, () => Array.from(view.unproxy(vm.filters)).map(filter =>
-				view.li({}, view.a({class: {selected: () => filter[0] === vm.filter}, attr: {href: '#/' + filter[0]}}, filter[1].title)))),
-			view.button({class: 'clear-completed', show: () => vm.completedCount, on: {click() {vm.model.clearCompleted()}}}, 'Clear completed')])]);
+		h('section', {class: 'main', show: () => vm.model.todos.length}, [
+			h('input', {class: 'toggle-all', attr: {id: 'toggle-all', type: 'checkbox'}, bind: {model: vm, key: 'allCompleted'}}),
+			h('label', {attr: {for: 'toggle-all'}}, 'Mark all as complete'),
+			h('ul', {class: 'todo-list', list: {array: () => vm.items, item: todo => TodoView(vm, view, h, todo)}})]),
+		h('footer', {class: 'footer', show: () => vm.model.todos.length}, [
+			h('span', {class: 'todo-count'}, () => [h('strong', {}, vm.remainingCount), ' ', view.pluralize('item', vm.remainingCount), ' ', 'left']),
+			h('ul', {class: 'filters'}, () => Array.from(view.unproxy(vm.filters)).map(filter =>
+				h('li', {}, h('a', {class: {selected: () => filter[0] === vm.filter}, attr: {href: '#/' + filter[0]}}, filter[1].title)))),
+			h('button', {class: 'clear-completed', show: () => vm.completedCount, on: {click() {vm.model.clearCompleted()}}}, 'Clear completed')])]);
 
 if (typeof module !== 'undefined') {
 	module.exports = {Todo, TodosModel, ViewModel, TodoView, TodoAppView};
